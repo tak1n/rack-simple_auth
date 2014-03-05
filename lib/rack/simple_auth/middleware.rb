ceb6d5e71d1416fe42a5116b567a0b042ff93856
Benny1992 <klotz.benjamin@yahoo.de>
2014-03-05 08:14:46 +0100

    placeholder method added

--- lib/rack/simple_auth/middleware.rb
+++ lib/rack/simple_auth/middleware.rb
@@ -7,21 +7,21 @@ module Rack
 
       def call(env)
         request = Rack::Request.new(env)
-        if valid?(request.env['HTTP_X_API_KEY'])
+        if valid?(request)
           @app.call(env)
         else
           response = Rack::Response.new("Unauthorized", 401, {'Content-Type' => 'text/html'})
-          response.finish          
+          response.finish
         end
       end
 
-      def valid?(key)
+      def valid?(request)
         # @todo : implement HMAC protocol
-        if key == "1"
-          true
-        else
-          false
-        end
+        secret = get_secret_from_configru_file
+
+        digest = OpenSSL::Digest::Digest.new('sha1')
+
+        true
       end
     end
   end
