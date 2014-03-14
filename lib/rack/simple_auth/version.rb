d8668d2d22f910b26903499568593370f4358934
Benny1992 <klotz.benjamin@yahoo.de>
2014-03-14 12:55:53 +0100

    bump version - logger implemented

--- lib/rack/simple_auth/version.rb
+++ lib/rack/simple_auth/version.rb
@@ -2,6 +2,6 @@ module Rack
   # Module which Contains different Authorization / Authentication Classes (HMAC, ..)
   module SimpleAuth
     # Current Gem Version
-    VERSION = '0.0.3'
+    VERSION = '0.0.4'
   end
 end
