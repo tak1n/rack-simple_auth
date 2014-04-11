b0a108e9235f53c7cacfa93f399b2c191d94ffa3
Benny1992 <klotz.benjamin@yahoo.de>
2014-04-11 13:28:41 +0200

    bumped version for refreshing gem dependencies

--- lib/rack/simple_auth/version.rb
+++ lib/rack/simple_auth/version.rb
@@ -2,6 +2,6 @@ module Rack
   # Module which Contains different Authorization / Authentication Classes (HMAC, ..)
   module SimpleAuth
     # Current Gem Version
-    VERSION = '0.0.8'
+    VERSION = '0.0.9'
   end
 end
