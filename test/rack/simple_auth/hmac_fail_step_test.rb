3c6c82b2928e8a354602fed937f26d6cad914b00
Benny <r3qnbenni@gmail.com>
2014-04-24 20:31:01 +0200

    don't test rack middlware itself, test init of rack middleware within capture_io to assert the output

--- test/rack/simple_auth/hmac_fail_step_test.rb
+++ test/rack/simple_auth/hmac_fail_step_test.rb
@@ -2,23 +2,20 @@ require 'test_helper.rb'
 
 # Test HMAC Authorization Method
 class HMACFailStepTest < MiniTest::Unit::TestCase
-  include Rack::Test::Methods
-
   def setup
     @secret = 'test_secret'
     @signature = 'test_signature'
   end
 
   def app
-    Rack::SimpleAuth.failstepapp
   end
 
   def test_fail_step
-    uri = '/'
-    content = { 'method' => 'GET', 'data' => uri }.to_json
-    hash = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), @secret, content)
+    out, err = capture_io do
+      Rack::Builder.parse_file("#{Rack::SimpleAuth.root}/test/config_fail_step.ru").first
+    end
 
-    get uri, {}, 'HTTP_AUTHORIZATION' => "#{hash}:#{@signature}"
+    assert_match('Warning: Minimum allowed stepsize is 0.01', out, 'Warning should be printed if stepsize is below 0.01')
   end
 
   def teardown
