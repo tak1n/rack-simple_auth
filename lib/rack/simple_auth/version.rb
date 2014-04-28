ed06a82c4cc0be90a2ac3cd6941b16d9a787c4e9
Benny <r3qnbenni@gmail.com>
2014-04-28 19:26:41 +0200

    bump up version for logger class

--- lib/rack/simple_auth/version.rb
+++ lib/rack/simple_auth/version.rb
@@ -2,6 +2,6 @@ module Rack
   # Module which Contains different Authorization / Authentication Classes (HMAC, ..)
   module SimpleAuth
     # Current Gem Version
-    VERSION = '0.1.3'
+    VERSION = '0.1.4'
   end
 end
