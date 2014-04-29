23125955e4af5459660df7b4d2bea4cafc5e3a81
Benny <r3qnbenni@gmail.com>
2014-04-29 16:07:37 +0200

    Middleware#log - don't use hash_array param, can be retreived via Middleware#allowed_messages

--- lib/rack/simple_auth/hmac/middleware.rb
+++ lib/rack/simple_auth/hmac/middleware.rb
@@ -7,6 +7,32 @@ module Rack
       # class Middleware
       # Middleware class which represents the interface to the rack api via Middleware#call
       # and checks if a request is hmac authorized.
+      #
+      # Usage:
+      #
+      # require 'rack/lobster'
+      # require 'rack/simple_auth'
+      #
+      # request_config = {
+      #   'GET' => 'path',
+      #   'POST' => 'params',
+      #   'DELETE' => 'path',
+      #   'PUT' => 'path',
+      #   'PATCH' => 'path'
+      # }
+      #
+      # use Rack::SimpleAuth::HMAC::Middleware do |options|
+      #   options.tolerance = 0.5
+      #   options.stepsize  = 0.01
+      #
+      #   options.secret = 'test_secret'
+      #   options.signature = 'test_signature'
+      #
+      #   options.logpath = "#{File.expand_path('..', __FILE__)}/logs"
+      #   options.request_config = request_config
+      # end
+      #
+      # run Rack::Lobster.new
       class Middleware
         def self.method_missing(name, *args)
           msg = "Did you try to use HMAC Middleware as Rack Application via 'run'?\n" if name.eql?(:call)
@@ -44,7 +70,7 @@ module Rack
         # @return [boolean] ValidationStatus [If authorized returns true, else false]
         def valid_request?
           if @request.env['HTTP_AUTHORIZATION'].nil?
-            log(allowed_messages)
+            log
 
             return false
           end
@@ -52,7 +78,7 @@ module Rack
           if request_signature == @config.signature && allowed_messages.include?(request_message)
             true
           else
-            log(allowed_messages)
+            log
 
             false
           end
@@ -109,21 +135,21 @@ module Rack
           end
         end
 
-        # Log to @logpath if request is unathorized
+        # Log to @config.logpath if request is unathorized
         # Contains:
         #   - allowed messages and received message
         #   - time when request was made
         #   - type of request
         #   - requested path
-        def log(hash_array)
+        def log
           if @config.logpath
             msg = "#{Time.new} - #{@request.request_method} #{@request.path} - 400 Unauthorized\n"
             msg << "HTTP_AUTHORIZATION: #{@request.env['HTTP_AUTHORIZATION']}\n"
-            msg << "Auth Message Config: #{@config[@request.request_method]}\n"
+            msg << "Auth Message Config: #{@config.request_config[@request.request_method]}\n"
 
-            if hash_array
+            if allowed_messages
               msg << "Allowed Encrypted Messages:\n"
-              hash_array.each do |hash|
+              allowed_messages.each do |hash|
                 msg << "#{hash}\n"
               end
             end
@@ -136,11 +162,13 @@ module Rack
 
         # Check if Stepsize is valid, if > min ensure stepsize is min stepsize
         # @param [float] min [minimum allowed stepsize]
+        # check @config.stepsize < min
         def valid_stepsize?(min)
           fail "Minimum allowed stepsize is #{min}" if @config.stepsize < min
         end
 
         # Check if tolerance is valid, tolerance must be greater than stepsize
+        # check @config.tolerance < @config.stepsize
         def valid_tolerance?
           if @config.tolerance < @config.stepsize
             fail "Tolerance must be greater than stepsize - Tolerance: #{@config.tolerance}, Stepsize: #{@config.stepsize}"
