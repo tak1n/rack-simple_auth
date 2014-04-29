a55ec96ddc0115d8f9488e70c8632549825ae024
Benny <r3qnbenni@gmail.com>
2014-04-29 15:23:54 +0200

    add dsl error handling

--- lib/rack/simple_auth/hmac/config.rb
+++ lib/rack/simple_auth/hmac/config.rb
@@ -6,6 +6,10 @@ module Rack
         attr_writer :secret, :signature
         attr_accessor :logpath, :request_config
 
+        def method_missing(name, *args)
+          fail "Unkown option #{name.to_s.gsub!('=', '')}"
+        end
+
         def tolerance
           @tolerance || 1
         end
