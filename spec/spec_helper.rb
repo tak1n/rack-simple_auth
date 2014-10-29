e1f477f11b6c18add05b08a28619be7f3a8b811e
Benny Klotz <r3qnbenni@gmail.com>
2014-10-29 07:30:39 +0100

    use minitest/pride

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,10 +22,11 @@ end
 # Minispec
 gem 'minitest'
 require 'minitest/autorun'
-require 'minitest/reporters'
+require 'minitest/pride'
+# require 'minitest/reporters'
 
-reporter_options = { color: true, slow_count: 5 }
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+# reporter_options = { color: true, slow_count: 5 }
+# Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 
 # Rack spec Methods
 require 'rack/test'
